798eadf27d8cf0530199ac17b7983bf7b84df014
Najifa <najifaanjum2015@gmail.com>
2022-12-14 03:45:57 +0600

    Java hello world api

---
+++ java_hello_world_api/src/main/java/com/javaguides/springboot/HelloController.java
@@ -0,0 +1,13 @@
+package com.javaguides.springboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "message hello";
+    }
+}
